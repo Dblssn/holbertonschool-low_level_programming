d25a9a4ae7b5e7c038a39e3e79b4a4aae0d27c9d
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-04-21 08:05:12 +0000

    task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,28 +1,22 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers 0 to 14 ten times
+ * more_numbers - prints numbers from 0 to 14, ten times
  *
  * Return: void
  */
 void more_numbers(void)
 {
-	int i, n;
+    int i, j;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (n = 0; n <= 14; n++)
-		{
-			if (n > 9)
-			{
-				_putchar('1');
-				_putchar(n % 10 + '0');
-			}
-			else
-			{
-				_putchar(n + '0');
-			}
-		}
-		_putchar('\n');
-	}
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j <= 14; j++)
+        {
+            if (j > 9)
+                _putchar('1');
+            _putchar((j % 10) + '0');
+        }
+        _putchar('\n');
+    }
 }
