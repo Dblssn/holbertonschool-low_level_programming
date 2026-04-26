83c7ad70133c2113fc2e958ca590b4ce005ea922
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-04-26 13:31:07 +0000

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to be printed
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	/* find length of string */
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	/* print in reverse */
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+
+	_putchar('\n');
+}
