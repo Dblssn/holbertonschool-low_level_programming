246ed134fb6d103afef85ee9b9ad0b80d197d90f
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-04-30 11:03:45 +0400

    task 0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+ int n;
+
+if (n>0) {
+printf ("number is positive\n");
+} else if (n<0) {
+printf ("number is negative\n");
+} else {
+printf ("number is 0\n";)
+}
+  }
