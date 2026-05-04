89e5b27e3f2daa5d610de7e22ef602366f52d0c5
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-05-04 14:22:22 +0400

    task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * swap_int - swaps the values of two integers
+  * @a: integer
+  * @b: integer
+  * Return: Always 0.
+  */
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+
+	*a = *b;
+	*b = c;
+}
