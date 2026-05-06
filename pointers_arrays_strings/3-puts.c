694338846e7b314e877d7665261829bd62e4d0fe
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-05-06 11:00:07 +0400

    task 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
 
 /**
-  * 3-puts - prints a string
-  
+  * _puts - prints a string
+  * @str: string to print
+  *
+  */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
