0948b0b60baae69334bbc4d6bf5b5b144870c03b
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-05-11 11:15:53 +0400

    task 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  * _strcat - copies a string from src to dest
+  *@dest: destination buffer
+  *@src: source string
+  * Return: pointing to the resulting dest
+  */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+		dest[a] = '\0';
+	}
+
+	return (dest);
+}
