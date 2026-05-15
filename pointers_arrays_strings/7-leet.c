5f546f3245efb158b9977f13e06fc9a487012496
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-05-15 12:21:53 +0400

    task 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,33 +9,24 @@
 char *leet(char *s)
 {
 	int i = 0;
+	int j;
+
+	char letters[11] = "aAeEoOtTlL";
+	char numbers[11] = "4433007711";
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-		{		s[i] = '4';
+		j = 0;
 
-		}
-
-		else if (s[i] == 'e' || s[i] == 'E')
-		{	s[i] = '3';
-
-		}
-
-		else if (s[i] == 'o' || s[i] == 'O')
+		while (letters[j] != '\0')
 		{
-			s[i] = '0';
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+			}
+			j++;
 		}
 
-		else if (s[i] == 't' || s[i] == 'T')
-		{
-			s[i] = '7';
-		}
-
-		else if (s[i] == 'l' || s[i] == 'L')
-		{
-			s[i] = '1';
-		}
 		i++;
 	}
 	return (s);
