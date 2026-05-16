4d993488447ab535c7821e7079d32eb7da365001
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-05-16 21:19:58 +0400

    task 8

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+int helper(int n, int i);
+
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: number to check
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return (helper(n, 2));
+}
+
+/**
+ * helper - checks divisibility recursively
+ * @n: number
+ * @i: divisor
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int helper(int n, int i)
+{
+	if (i * i > n)
+		return (1);
+
+	if (n % i == 0)
+		return (0);
+
+	return (helper(n, i + 1));
+}
