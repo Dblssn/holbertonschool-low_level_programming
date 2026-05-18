321a87c9d79598061fc1daca1b9477549dc8b8d6
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-05-18 13:58:30 +0400

    task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,1 +1,21 @@
+/**
+ * is_prime_number - checks if an integer is a prime number
+ * @n: number to check
+ *
+ * Return: 1 if n is prime, otherwise 0
+ */
+int is_prime_number(int n)
+{
+	int i;
 
+	if (n <= 1)
+		return (0);
+
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+
+	return (1);
+}
