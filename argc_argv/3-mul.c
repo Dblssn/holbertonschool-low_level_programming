86e89bfa6a58016571d3ed0c0309d1073a54367c
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-05-19 13:31:44 +0400

    task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
@@ -10,6 +18,5 @@ int main(int argc, char *argv[])
 	}
 
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-
 	return (0);
 }
