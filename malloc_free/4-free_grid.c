9d340c46f8be677af1ac816fe37750f5e237d489
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-05-22 09:37:05 +0400

    task 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2D grid previously created
+ * @grid: pointer to the grid
+ * @height: number of rows
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
