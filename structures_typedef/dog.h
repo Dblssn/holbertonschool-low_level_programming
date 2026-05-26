e59f2e6449895c5b9fb2c43b6bee99d74849aaa6
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-05-26 11:34:45 +0400

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  *struct dog - new type
+  *@age: age
+  *@name: name
+  *@owner: owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
