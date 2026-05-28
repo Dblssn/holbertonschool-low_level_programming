48db4f89bf97b00840e495383e510b6d29c3da64
Dblssn <ramsurrunbalissonsaagarikha.com>
2026-05-28 14:06:30 +0400

    task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+  *print_name - prints name
+  *@name: name
+  *@f: function pointers
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return;
+
+	if (f == NULL)
+		return;
+	f(name);
+}
